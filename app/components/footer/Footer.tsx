900bc43376687011a02ead392375fd4c47bc0bb8
Ömer Faruk Özler <95633424+omerfarukozler@users.noreply.github.com>
2024-08-25 16:49:22 +0300

    Products Added

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="w-full h-24 bg-orange-600 text-slate-100 flex items-center justify-center mt-3 md:mt-10">
+     Daha sonra düzenlenecek
     </div>
   )
 }
--- app/components/home/Banner.tsx
+++ app/components/home/Banner.tsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className="h-[237px] flex items-center justify-center">
     <div className="h-[220px] relative w-full">
-    <Image src={"/kutu.jpg"} fill alt=""/>
+    <Image src={"/kutu.jpg"} fill alt="" className="object-cover"/>
     </div>
     </div>
   )
