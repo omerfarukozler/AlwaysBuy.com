b93abe8273212aa3efa3de64519a97715b56e75c
Ömer Faruk Özler <95633424+omerfarukozler@users.noreply.github.com>
2024-08-29 16:51:10 +0300

    Button and Counter.tsx Added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Banner from "./components/home/Banner";
 import Category from "./components/home/Category";
 import Products from "./components/home/Products";
 
+
 export default function Home() {
   return (
   <div>
